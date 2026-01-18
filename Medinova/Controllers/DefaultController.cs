72802c27358014ec1179ab38bf178c44d18f9e22
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2026-01-19 00:31:10 +0300

    Refactor appointment booking with dynamic doctor loading
    Refactored DefaultController to use direct context instantiation and updated appointment actions. Added AJAX endpoint to fetch doctors by department. Updated DefaultAppointment view to use dynamic dropdowns, bind form fields to the model, and load doctors via jQuery. Simplified Times.cs to only include time constants.

--- Medinova/Controllers/DefaultController.cs
+++ Medinova/Controllers/DefaultController.cs
@@ -6,12 +6,7 @@ namespace Medinova.Controllers
 {
     public class DefaultController : Controller
     {
-        private readonly MedinovaContext _context;
-
-        public DefaultController(MedinovaContext context)
-        {
-            _context = context;
-        }
+        MedinovaContext context = new MedinovaContext();
 
         // GET: Default
         public ActionResult Index()
@@ -22,32 +17,35 @@ namespace Medinova.Controllers
         [HttpGet]
         public PartialViewResult DefaultAppointment()
         {
-            return PartialView();
-        }
-
-        [HttpPost]
-        public ActionResult MakeAppointment(Appointment appointment)
-        {
-            var departments = _context.Departments.ToList();
+            var departments = context.Departments.ToList();
             ViewBag.departments = (from department in departments
                                    select new SelectListItem
                                    {
                                        Text = department.Name,
                                        Value = department.DepartmentId.ToString()
                                    }).ToList();
+            return PartialView();
+        }
 
-            var doctors = _context.Doctors.ToList();
-            ViewBag.doctors = (from doctor in doctors
-                               select new SelectListItem
-                               {
-                                   Text = doctor.FullName,
-                                   Value = doctor.DoctorId.ToString()
-                               }).ToList();
+        [HttpPost]
+        public ActionResult MakeAppointment(Appointment appointment)
+        {
 
-
-            _context.Appointments.Add(appointment);
-            _context.SaveChanges();
+            context.Appointments.Add(appointment);
+            context.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public JsonResult GetDoctorByDepartmentId(int departmentId)
+        {
+            var doctors = context.Doctors.Where(x=>x.DepartmentId == departmentId)
+                                           .Select(doctor => new SelectListItem
+                                           {
+                                               Text = doctor.FullName,
+                                               Value = doctor.DepartmentId.ToString()
+                                           }).ToList();
+
+            return Json(doctors,JsonRequestBehavior.AllowGet);
         }
     }
 }
