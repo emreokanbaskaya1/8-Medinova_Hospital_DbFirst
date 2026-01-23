bf973b512c89c52f40480f00334cba024faeeb44
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2026-01-24 01:45:06 +0300

    Add user authentication with login/logout and User entity
    Implemented Forms Authentication with AccountController for login/logout, added User entity to the data model, and created Login view and DTO. Configured global authorization, updated navigation with sign out, and added AdminAboutController. Minor model update to AppointmentDate type. Updated project files accordingly.

---
+++ Medinova/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Medinova.DTOs;
+using Medinova.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Medinova.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        MedinovaContext context = new MedinovaContext();
+
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(LoginDto model)
+        {
+            var user = context.Users.FirstOrDefault(x=>x.UserName == model.UserName && x.Password == model.Password);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Username or Password is not correct.");
+                return View(model);
+            }
+
+            FormsAuthentication.SetAuthCookie(user.UserName, false);
+            Session["userName"] = user.UserName;
+            Session["fullName"] = user.FirstName + " " + user.LastName;
+            return RedirectToAction("Index", "AdminAbout");
+        }
+
+        public ActionResult Logout() 
+        { 
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+
+            return RedirectToAction("Login");
+        
+        }
+    }
+}
